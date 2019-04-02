6e06cd1015b3f23404e4617af7c78a41648b7a4b
Thomas Gielfeldt <thomas@gielfeldt.dk>
2019-04-02 12:32:47 +0200

    Add regex matching to proxymapping

--- src/resolver/subnet.go
+++ src/resolver/subnet.go
@@ -48,46 +48,32 @@ func (s *Subnet) parseProxyMappings(mappings string) map[string]string {
 	return proxyMap
 }
 
-/*
-func (pn *Subnet) splitHostHostPort(str string) (srcHost string, dstHost string, dstPort int, err error) {
-	hhp := strings.Split(str, ":")
-
-	port, _ := strconv.Atoi(hhp[len(hhp)-1])
-	if port == 0 {
-		port = 80
-		hhp = append(hhp, "80")
-	}
-	if len(hhp) > 3 {
-		return "", "", -1, errors.New("Wrong format '%s' expected [srchost:]dsthost[:destport]")
-	}
-	if len(hhp) == 2 {
-		hhp = []string{hhp[0], hhp[0], hhp[1]}
-	}
-	return hhp[0], hhp[1], port, nil
-}
-
-func (pn *Subnet) SetProxyMappings(mappings []string) {
-	pn.proxyMappings = make(map[string]string)
-	for _, hostport := range mappings {
-		src, dst, port, err := pn.splitHostHostPort(hostport)
-		if err != nil {
-			log.Fatal(fmt.Sprintf("Error parsing proxy mapping: %s - %v", hostport, err))
-		}
-		pn.proxyMappings[src] = fmt.Sprintf("%s:%d", dst, port)
-	}
-}
-*/
 func (s *Subnet) GetDestinationHostPort(sourceHostPort string) (dstHostPort string, err error) {
 	sourceHost := strings.Split(sourceHostPort, ":")[0]
+
+	// Full host matching
 	if dstHostPort, ok := s.proxyMappings[sourceHost]; ok {
 		return dstHostPort, nil
 	}
+
+	// First part of host matching
 	srcHost := strings.Split(sourceHost, ".")[0]
 	if dstHostPort, ok := s.proxyMappings[srcHost]; ok {
 		return dstHostPort, nil
 	}
+
+	// Arbitrary number of host parts matching
+	for src, dst := range s.proxyMappings {
+		if strings.HasPrefix(sourceHost, src+".") {
+			return dst, nil
+		}
+	}
+
+	// Don't assume fallback, if we only proxy mapped hosts
 	if s.proxyOnlyMappedHosts {
 		return "", errors.New(fmt.Sprintf("Only configured gateways allowed ('%s' not found)", srcHost))
 	}
+
+	// Fallback, assume first part host exists
 	return fmt.Sprintf("%s:%d", srcHost, 80), nil
 }
